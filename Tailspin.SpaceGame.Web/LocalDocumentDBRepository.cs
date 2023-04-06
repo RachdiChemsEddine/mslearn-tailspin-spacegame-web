821dce771658c458ad7b3043d22c69b9b62d9b53
RachdiChemsEddine <Chems_Eddine.RACHDI@etu.uca.fr>
2023-04-06 10:29:34 +0200

    Return correct number of items

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -55,15 +55,15 @@ namespace TailSpin.SpaceGame.Web
             Func<T, int> orderDescendingPredicate,
             int page = 1, int pageSize = 10
         )
-        {
-            var result = _items
-                .Where(queryPredicate) // filter
-                .OrderByDescending(orderDescendingPredicate) // sort
-                .Skip(page * pageSize) // find page
-                .Take(pageSize - 1); // take items
+            {
+                var result = _items
+                    .Where(queryPredicate) // filter
+                    .OrderByDescending(orderDescendingPredicate) // sort
+                    .Skip(page * pageSize) // find page
+                    .Take(pageSize); // take items
 
-            return Task<IEnumerable<T>>.FromResult(result);
-        }
+                return Task<IEnumerable<T>>.FromResult(result);
+            }
 
         /// <summary>
         /// Retrieves the number of items that match the given query predicate.
